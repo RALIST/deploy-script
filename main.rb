85e19241df4b70fa1faf04de832bf84b47d520f1
ralist <danilov@ralist.ru>
2023-03-04 14:37:41 +0400

    config.rb

--- main.rb
+++ main.rb
@@ -13,18 +13,20 @@ config['servers'].compact.each do |server|
   commands = config['commands'] || []
   commands += server['commands'] || []
   raw_commands = commands.join(' && ')
+  server_data = []
 
   Net::SSH.start(options.delete(:host), options.delete(:user), **options) do |ssh|
     channel = ssh.open_channel do |ch|
+      puts "Executing commands in server #{server['host']}..."
       ch.exec raw_commands
     end
 
     channel.on_data do |_ch, data|
-      puts data
+      server_data << data
     end
 
     channel.on_extended_data do |_ch, _type, data|
-      puts data
+      server_data << data
     end
 
     channel.on_close do
@@ -32,5 +34,7 @@ config['servers'].compact.each do |server|
     end
 
     channel.wait
+
+    File.open("log/#{server['host']}.log", "w") { |file| file.write(server_data.join)}
   end
 end
